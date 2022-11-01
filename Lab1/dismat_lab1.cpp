0e5669e257a437090e62d58b8c29074f4f6aa575
dominik <dominik.barukcic@fer.hr>
2022-11-01 15:27:42 +0100

    improved behavior for bad test cases

--- Lab1/dismat_lab1.cpp
+++ Lab1/dismat_lab1.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 double solveFor (double b[], double c[], int num) {
     if(b[1] * c[0] - b[0] * c[1] == 0) {
-        cout << "-" << endl;
+        cout << "Sustav jednadzbi rekurzivnih relacija za predane clanove nema rjesenje (λ1, λ2)." << endl;
         exit(0);
     } 
     double lambda1 = (b[2] * c[0] - b[0] * c[2]) / (b[1] * c[0] - b[0] * c[1]);
@@ -35,6 +35,10 @@ int main(void) {
     double b[3], c[3];
     cout << "Unesite nenegativan cijeli broj: ";
     cin >> num;
+    if(num < 0) {
+        cout << "Neispravan argument." << endl;
+        return 1;
+    }
     for(auto i = 0; i < 3; i++) {
         cout << "Unesite vrijednost broja b_" << i << ": ";
         cin >> b[i];
@@ -43,6 +47,7 @@ int main(void) {
         cout << "Unesite vrijednost broja c_" << i << ": ";
         cin >> c[i];
     };
-    cout << "Vrijednost broja b_n: " << solveFor(b, c, num) << endl;
+    double result = solveFor(b, c, num);
+    cout << "Vrijednost broja b_n: " << result << endl;
     return 0;
 }
